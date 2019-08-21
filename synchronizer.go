17d9cd93f5f8d2995a4c6a41e06780962ba6d6dc
Johnny Horvi <johnny@horvi.no>
2019-08-21 10:54:35 +0200

    print error message from getDesiredRolebindings

--- synchronizer.go
+++ synchronizer.go
@@ -61,7 +61,10 @@ func (s *Synchronizer) synchronizeRBAC() {
 			continue
 		}
 
-		desired, _ := s.getDesiredRoleBindings(s.getTargetNamespaces())
+		desired, err := s.getDesiredRoleBindings(s.getTargetNamespaces())
+		if err != nil {
+			log.Errorf("unable to get desired rolebindings", err)
+		}
 
 		// Managed bindings that exist in cluster, but is not part of the configuration
 		orphans := diff(desired, current)
@@ -177,7 +180,7 @@ func getMatchingRoleBinding(roleBinding v1.RoleBinding, roleBindings []v1.RoleBi
 		}
 
 	}
-	return nil, fmt.Errorf("unable to find matching rolebinding, this is bad")
+	return nil, fmt.Error("unable to find matching rolebinding, this is bad")
 }
 
 func hasDifferentSubjects(s1 []v1.Subject, s2 []v1.Subject) bool {
